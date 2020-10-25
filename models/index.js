e0ef69fdceafef0172e1ab65fbace627c542fa16
Pierre Tran <pierre.tran.99@gmail.com>
2020-10-25 01:31:55 -0400

    changed version

--- models/index.js
+++ models/index.js
@@ -1,25 +1,23 @@
-var Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 // Connect to CockroachDB through Sequelize.
-var sequelize = new Sequelize('parking', 'dandyuser', 'test', {
-    host:"dandy-293601:northamerica-northeast1:dandydb",
-    dialect: 'mysql',
-    port: 3306,
-    logging: false,
+const sequelize = new Sequelize("parking", "dandyuser", "test", {
+  host: "dandy-293601:northamerica-northeast1:dandydb",
+  dialect: "mysql",
+  port: 3306,
+  logging: false,
+});
+const models = {
+  account: sequelize.import("./account"),
+  parkingspot: sequelize.import("./parkingspot"),
+};
+
+Object.keys(models).forEach((modelName) => {
+  if ("associate" in models[modelName]) {
+    models[modelName].associate(models);
+  }
 });
 
-const models = {
-    account: sequelize.import("./account"),
-    parkingspot: sequelize.import("./parkingspot")
-  };
-  
-  Object.keys(models).forEach((modelName) => {
-    if ("associate" in models[modelName]) {
-      models[modelName].associate(models);
-    }
-  });
-  
-  models.sequelize = sequelize;
-  models.Sequelize = Sequelize;
-  module.exports = models;
-  
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+module.exports = models;
